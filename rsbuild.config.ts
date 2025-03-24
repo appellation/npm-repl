4e4476f529df17f055ef258a4420349c8c736986
Will Nelson <me@wnelson.dev>
2025-03-23 20:41:12 -0700

    css

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { UnoCSSRspackPlugin } from "@unocss/webpack/rspack";
 import MonacoWebpackPlugin from "monaco-editor-webpack-plugin";
+import { presetWind4 } from "unocss";
 
 export default defineConfig({
 	plugins: [pluginReact()],
 	tools: {
 		rspack: {
-			plugins: [new MonacoWebpackPlugin({ languages: ["javascript"] })],
+			plugins: [
+				new MonacoWebpackPlugin({ languages: ["javascript"] }),
+				UnoCSSRspackPlugin({
+					presets: [presetWind4()],
+				}),
+			],
 		},
 	},
 	server: {
--- src/Packages.tsx
+++ src/Packages.tsx
@@ -6,60 +6,60 @@ import { webContainerAtom } from "./state";
 import { readAll } from "./util";
 
 function PackageList() {
-  const [webContainer] = useAtom(webContainerAtom);
+	const [webContainer] = useAtom(webContainerAtom);
 
-  const { data, error, isPending } = useQuery({
-    queryKey: ["npm", "ls"],
-    async queryFn() {
-      const process = await webContainer.spawn("npm", ["ls", "--no-color"]);
-      const output = readAll(process.output, process.exit);
+	const { data, error, isPending } = useQuery({
+		queryKey: ["npm", "ls"],
+		async queryFn() {
+			const process = await webContainer.spawn("npm", ["ls", "--no-color"]);
+			const output = readAll(process.output, process.exit);
 
-      await process.exit;
-      return output;
-    },
-  });
-  console.log({ data, error, isPending });
+			await process.exit;
+			return output;
+		},
+	});
+	console.log({ data, error, isPending });
 
-  return <pre>{data}</pre>;
+	return <pre>{data}</pre>;
 }
 
 export default function Packages() {
-  const [webContainer] = useAtom(webContainerAtom);
-  const queryClient = useQueryClient();
+	const [webContainer] = useAtom(webContainerAtom);
+	const queryClient = useQueryClient();
 
-  const { mutate, isPending } = useMutation({
-    async mutationFn(pkgName: string) {
-      const process = await webContainer.spawn("npm", ["install", pkgName]);
-      return process.exit;
-    },
-    async onSuccess() {
-      // @ts-expect-error tanstack query incorrect types
-      await queryClient.invalidateQueries([{ queryKey: ["npm", "ls"] }]);
-    },
-  });
+	const { mutate, isPending } = useMutation({
+		async mutationFn(pkgName: string) {
+			const process = await webContainer.spawn("npm", ["install", pkgName]);
+			return process.exit;
+		},
+		async onSuccess() {
+			// @ts-expect-error tanstack query incorrect types
+			await queryClient.invalidateQueries([{ queryKey: ["npm", "ls"] }]);
+		},
+	});
 
-  const handleAddPackage = useCallback(
-    (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
+	const handleAddPackage = useCallback(
+		(e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
 
-      const data = new FormData(e.currentTarget);
-      mutate(data.get("name") as string);
-    },
-    [mutate]
-  );
+			const data = new FormData(e.currentTarget);
+			mutate(data.get("name") as string);
+		},
+		[mutate],
+	);
 
-  return (
-    <div>
-      <PackageList />
-      <Form onSubmit={handleAddPackage}>
-        <TextField>
-          <Label>Package Name</Label>
-          <Input name="name" />
-        </TextField>
-        <Button type="submit" isPending={isPending}>
-          Add
-        </Button>
-      </Form>
-    </div>
-  );
+	return (
+		<div>
+			<PackageList />
+			<Form onSubmit={handleAddPackage}>
+				<TextField>
+					<Label>Package Name</Label>
+					<Input name="name" />
+				</TextField>
+				<Button type="submit" isPending={isPending}>
+					Add
+				</Button>
+			</Form>
+		</div>
+	);
 }
